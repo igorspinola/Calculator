382227d155bc27f501fd19d78a688c40b8757220
Igor <igorspinoladev@gmail.com>
2024-06-17 18:19:16 -0300

    css finished

---
+++ calculator.js
@@ -0,0 +1,6 @@
+const digits = document.querySelectorAll(".digit")
+const operations = document.querySelectorAll(".operation")
+
+console.log(digits)
+console.log(operations)
+console.log(operations[9])
