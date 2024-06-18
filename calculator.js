1f4891212a7b90230665ec35fff50f9525fe8e27
Igor <igorspinoladev@gmail.com>
2024-06-17 21:41:41 -0300

    code organized

--- calculator.js
+++ calculator.js
@@ -1,7 +1,6 @@
 const digits = document.querySelectorAll(".digit")
 const operations = document.querySelectorAll(".operation")
 const string_operations = ["+", "-", "x", "%"]
-console.log(string_operations)
 const input = document.querySelector("input")
 const equal = document.querySelector(".equal")
 const clear = document.querySelector(".clear")
@@ -10,17 +9,11 @@ const dot = document.querySelector(".dot")
 let operation = ""
 let first_number = ""
 let second_number = ""
-console.log(operation)
-console.log(digits)
-console.log(operations)
-console.log(operations[9])
 
 digits.forEach(
     (d) => {
-        d.addEventListener("click", (e) => {
-            console.log(e)
-            console.log(d)
-            if(string_operations.includes(input.value)) {
+        d.addEventListener("click", () => {
+            if (string_operations.includes(input.value)) {
                 input.value = ""
                 input.value += d.innerText
             } else {
@@ -36,17 +29,15 @@ operations.forEach(
             operation = o.textContent
             first_number = input.value       
             input.value = operation
-            console.log(first_number)
-            console.log(operation)
         })
     }
 )
 
 equal.addEventListener("click", () => {
     second_number = input.value
-    console.log(second_number)
     first_number = Number(first_number)
     second_number = Number(second_number)
+
     switch(operation) {
         case "+":
             input.value = `${first_number + second_number}`
@@ -61,6 +52,7 @@ equal.addEventListener("click", () => {
             input.value = `${first_number / second_number}` 
             break
     }
+
     const result = input.value
     first_number = result
     second_number = ""
@@ -72,7 +64,6 @@ clear.addEventListener("click", () => {
     first_number = ""
     second_number = ""
     operation = ""
-
 })
 
 delete_button.addEventListener("click", () => input.value = input.value.slice(0, input.value.length - 1))
